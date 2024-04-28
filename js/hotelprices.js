37dda359d5640181a8c15a1a56adae45d1fcf0be
Mohamed_Elayech <mohamed.elayech1@gmail.com>
2024-04-28 10:54:00 +0100

    animationg hotels

---
+++ js/hotelprices.js
@@ -0,0 +1,14 @@
+var single=document.querySelector('#single')
+var double=document.querySelector('#double')
+var triple=document.querySelector('#triple')
+var quadruple=document.querySelector('#quadruple')
+var price=document.querySelector("#price1")
+
+var option=document.querySelector(".hotel_select").ariaSelected.value;
+function chose(){
+switch(option){
+    case "Breakfast":
+        price.innerHTML="000"
+
+}
+}
